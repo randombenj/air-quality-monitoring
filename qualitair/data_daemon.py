2fdcb9727ddfb32aeeb8c91d964e1d62f124368a
Benj Fassbind <randombenj@gmail.com>
2021-01-24 15:58:31 +0100

    Make sensor init async

--- qualitair/data_daemon.py
+++ qualitair/data_daemon.py
@@ -19,14 +19,25 @@ class DataDaemon():
 
     async def run(self):
         logging.info("initialize the sensor")
-        self._sensor.start_measurement()
+        # as the `self._sensor.start_measurement()` would be
+        # blocking, it is reimplemented in the loop
+        # and will only start recording measurements
+        # when the sensor is heated up.
+        is_inited = False
+        testsamples = 0
 
         while self._run:
             result = self._sensor.get_air_quality()
             logging.info(result)
 
-            await Measurement.create(
-                co2=result.equivalent_co2,
-                voc=result.total_voc
-            )
+            if is_inited:
+                await Measurement.create(
+                    co2=result.equivalent_co2,
+                    voc=result.total_voc
+                )
+            else:
+                testsamples = testsamples + 1
+                if result.equivalent_co2 != 400 or result.total_voc != 0 or testsamples > 20:
+                    is_inited = True
+
             await asyncio.sleep(1)
